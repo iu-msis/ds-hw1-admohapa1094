a518f6bb942306cd6bbecea0a0b9e080724bed82
admohapa1094 <admohapa@iu.edu>
2018-10-09 15:38:23 -0400

    fixed error in common.php

--- app/common.php
+++ app/common.php
@@ -4,14 +4,12 @@
 chdir(__DIR__);
 set_include-path (__DIR__);
 
-require 'environment.php'
-
 // Require class files
 if ($_SERVER['REQUEST_METHOD'] == 'POST'
 && stripos ($_SERVER['CONTENT_TYPE'], 'application/json') !== false ) {
   $_POST = json_decode (file_getcontents('php://input'), true);
 }
+require 'environment.php'
 
 /** MODELS **/
-require 'models/Work.php';
-require 'models/Team.php';
+require 'models/Comment.php';
