f07fcb2fecb28e4e0ba9fa87a01b17a8d194fc57
admohapa1094 <admohapa@iu.edu>
2018-10-09 17:54:06 -0400

    spelling error...

--- app/common.php
+++ app/common.php
@@ -7,7 +7,7 @@ set_include_path (__DIR__);
 // Require class files
 if ($_SERVER['REQUEST_METHOD'] == 'POST'
 && stripos ($_SERVER['CONTENT_TYPE'], 'application/json') !== false ) {
-  $_POST = json_decode (file_getcontents('php://input'), true);
+  $_POST = json_decode (file_get_contents('php://input'), true);
 }
 require 'environment.php';
 
