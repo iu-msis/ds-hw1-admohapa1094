44001d73cfa9b02b3f72cad9808b7bf7093b80c9
admohapa1094 <admohapa@iu.edu>
2018-10-15 14:34:57 -0400

    debugging 1

--- public/js/comment.app.js
+++ public/js/comment.app.js
@@ -14,7 +14,9 @@ var commentApp = new Vue ({
 				// console.log(document.getElementById("feedbackComment").value);
 	      fetch('http://ec2-35-161-167-109.us-west-2.compute.amazonaws.com/api/comment.php')
 	      .then(response => response.json())
-	      .then (json => {commentApp.comments = json})
+	      .then (json => {commentApp.comments = json
+        })
+        .then(resp => {console.log(resp)})
 	      .catch( function(err){
 	        console.log(err)
 	      })
