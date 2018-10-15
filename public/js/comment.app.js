e981bbc7e6b413539d780c728f952f96240c0ad3
admohapa1094 <admohapa@iu.edu>
2018-10-15 14:24:46 -0400

    Creating front-end for comment api

---
+++ public/js/comment.app.js
@@ -0,0 +1,27 @@
+var commentApp = new Vue ({
+    el: '#commentData' ,
+    data: {
+      comments:
+      [
+        {
+          id:'',
+          comment:''
+        }
+      ]
+    },
+    methods: {
+      fetchComments() {
+				// console.log(document.getElementById("feedbackComment").value);
+	      fetch('http://ec2-35-161-167-109.us-west-2.compute.amazonaws.com/api/comment.php')
+	      .then(response => response.json())
+	      .then (json => {commentApp.comments = json})
+	      .catch( function(err){
+	        console.log(err)
+	      })
+	    }
+    },
+    created: function(){
+        this.fetchComments()
+    }
+
+})
